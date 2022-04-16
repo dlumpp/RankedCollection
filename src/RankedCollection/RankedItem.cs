853a78fc5f833d47047848d5689fdb4823bceaba
Dan Lumpp <20948500+dlumpp@users.noreply.github.com>
2022-04-16 15:59:30 -0400

    get rid of IRankedItem<T>

--- src/RankedCollection/RankedItem.cs
+++ src/RankedCollection/RankedItem.cs
@@ -1,18 +1,7 @@
 ﻿namespace EnhancedCollections;
 
-public interface IRankedItem<T>
+public class RankedItem
 {
-    T Item { get; }
-    int Rank { get; set; }
-
-    void Promote();
-    void Demote();
-}
-
-internal class RankedItem<T> : IRankedItem<T> where T : notnull
-{
-    public T Item { get; }
-
     int _rank;
     public int Rank
     {
@@ -23,31 +12,34 @@ internal class RankedItem<T> : IRankedItem<T> where T : notnull
             {
                 //TODO: normalize entered rank if out of bounds
                 int? newRank = RankChanged?.Invoke(this, value);
-                if(newRank.HasValue)
+                if (newRank.HasValue)
                     _rank = newRank.Value;
             }
         }
     }
 
-    public event RankChangedEvent? RankChanged;
-    public delegate int RankChangedEvent(IRankedItem<T> item, int desiredRank);
+    public void Promote() => Rank--;
+    public void Demote() => Rank++;
 
-    public RankedItem(T item, int rank)
-    {
-        Item = item;
-        _rank = rank;
-    }
+    internal event RankChangedEvent? RankChanged;
+    internal delegate int RankChangedEvent(RankedItem item, int desiredRank);
 
     /// <summary>
     /// Use to cascade rank shifts through list without firing infinite events
     /// </summary>
     /// <param name="i"></param>
-    public void SetRank(int i) => _rank = i;
+    internal void SetRank(int i) => _rank = i;
+}
 
-    public void Promote() => Rank--;
+public class RankedItem<T> : RankedItem
+{
+    public T Item { get; }
 
-    public void Demote() => Rank++;
-
+    internal RankedItem(T item)
+    {
+        Item = item;
+    }
 
     public static implicit operator T(RankedItem<T> rankedItem) => rankedItem.Item;
+    public static implicit operator RankedItem<T>(T item) => new RankedItem<T>(item);
 }
